e56e7dc5df4e757e55d6a9e3584fe2f0d18bf3a1
Krishna <krishnapatil1412@gmail.com>
2026-07-23 18:30:39 +0530

    Time: 0 ms (100%), Space: 12.6 MB (77.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def hIndex(self, citations):
+        n = len(citations)
+        citations.sort()
+
+        for i,v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0
