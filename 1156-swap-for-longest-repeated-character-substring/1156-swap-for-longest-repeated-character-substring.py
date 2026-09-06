b57b0bea6ae79012ab64e999f1e6144906640479
Krishna <krishnapatil1412@gmail.com>
2026-09-06 16:40:53 +0530

    Time: 215 ms (7.69%), Space: 13.3 MB (71.79%) - LeetHub

---
+++ 1156-swap-for-longest-repeated-character-substring/1156-swap-for-longest-repeated-character-substring.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxRepOpt1(self, text):
+        total = Counter(text)
+        n = len(text)
+        ans = 0
+        
+        for ch in total:
+            left = 0
+            count = 0  
+            
+            for right in range(n):
+                if text[right] == ch:
+                    count += 1
+                
+                while (right - left + 1) - count > 1:
+                    if text[left] == ch:
+                        count -= 1
+                    left += 1
+                
+                ans = max(ans, min(right - left + 1, total[ch]))
+        
+        return ans
